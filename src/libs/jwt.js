56453fbf6df30811564b4fc4a1854104cbadc6cd
unknown <pepodiaz777@gmail.com>
2024-06-03 21:20:57 +0200

    Commit Inicial

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -8,6 +8,7 @@ dotenv.config();
 // Function to provide user token
 export const generateTokenById = (userId) =>
 {
+    console.log('clave secreta recibida: ' + process.env.JWT_SECRET);
     console.log('GENERANDO TOKEN PARA EL ID: ' + userId);
     return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1d' })
 }
