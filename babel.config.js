ca0fc17ecafb5ebb1d285b2b483c6060746042c6
unknown <pepodiaz777@gmail.com>
2024-06-03 19:50:04 +0200

    Commit Inicial 2

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-    "presets": [
-        "@babel/preset-env"
+export default {
+    presets: [
+        ['@babel/preset-env', { 
+            targets: { node: 'current' } 
+        }]
     ]
-}
+};
