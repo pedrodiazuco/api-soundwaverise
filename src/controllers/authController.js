628adc0ed9e0695065573e251836f5fe1cea0d07
unknown <pepodiaz777@gmail.com>
2024-06-03 21:37:18 +0200

    Commit Inicial

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -48,7 +48,7 @@ const signIn = async (req, res) => {
                     maxAge: 24 * 60 * 60 * 1000 // 24 horas en milisegundos
                 });
                 console.log('Enviando cookie:')
-                console.log(res.cookies);
+                console.log(res.cookie);
                 return res.status(200).json({ user: dataSession });
             }
         }
