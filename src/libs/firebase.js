e70cf3b68fcd4ee3c2c7b22ac75f0281e2884f74
unknown <pepodiaz777@gmail.com>
2024-06-03 22:29:03 +0200

    Commit Inicial

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -3,11 +3,12 @@ import admin from 'firebase-admin';
 
 dotenv.config();
 
+console.log(process.env.FIREBASE_PRIVATE_KEY);
 admin.initializeApp({
         credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
